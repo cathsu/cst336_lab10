ba454633fd447da7964fa7e65687ad8b95ac3bde
EC2 Default User <ec2-user@ip-172-31-36-239.us-east-2.compute.internal>
2020-04-28 07:25:52 +0000

    Commit lab

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 /* Require external APIs and start our application instance */
 var express = require('express');
 var mysql = require('mysql');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var bcrypt = require('bcrypt');
 var app = express();
 
 /* Configure our server to read public folder and ejs files */
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(session({
+    secret: 'top secret code!',
+    resave: true,
+    saveUninitialized: true
+}));
 app.set('view engine', 'ejs');
 
 /* Configure MySQL DBMS */
 const connection = mysql.createConnection({
-    host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'm6p63nd8yypcno2c',
-    password: 'v4f5mzdjd1b6tlpm',
-    database: 'wgqij9oon6gipvu9', 
+    host: 'localhost',//'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'cathsu',//'m6p63nd8yypcno2c',
+    password: 'cathsu' ,//'v4f5mzdjd1b6tlpm',
+    database: 'quotes_db' ,//'wgqij9oon6gipvu9', 
     multipleStatements: true
 });
 
@@ -32,10 +43,34 @@ app.get('/', function(req, res){
             categories = found[0]; 
             authors = found[1]; 
         }
-        res.render('home', {categories: categories, authors: authors});
+        res.render('home', {categories: categories, authors: authors, isLoggedIn: req.session.authenticated});
     }); 
     
     
+});
+
+app.get('/login', function(req, res) {
+    res.render('login');    
+}); 
+
+
+app.post('/login', function(req, res){
+    console.log("username = " + req.body.username + " password = " + req.body.password);
+    let isLoginValid = checkLoginCredentials(req.body.username, req.body.password); 
+    console.log(isLoginValid);
+    if(isLoginValid){
+        req.session.authenticated = true;
+        req.session.user = req.body.username;
+        res.redirect('/author');
+    }
+    else{
+        res.render('login', {error: true});
+    }
+});
+
+app.get('/logout', function(req, res){
+   req.session.destroy();
+   res.redirect('/');
 });
 
 app.get('/quotes', function(req, res) {
@@ -48,17 +83,134 @@ app.get('/quotes', function(req, res) {
     
 });
 
+app.get("/author", isAuthenticated, function(req, res) {
+    var stmt = 'SELECT * FROM l9_author;';
+    console.log(stmt);
+    var authors = null;
+    connection.query(stmt, function(error, results){
+        if(error) {
+            console.log("error in /author");
+            throw error;
+        }
+        if(results.length) authors = results;
+        // console.log(authors);
+        res.render('author', {authors: authors});
+    });
+}); 
 
-app.get("/author/:id", function(req, res) {
+app.get("/author/:aid/bio", isAuthenticated, function(req, res) {
+    var stmt = 'SELECT * FROM l9_author WHERE authorId=' + req.params.aid + ';';
+    console.log(stmt);
+    connection.query(stmt, function(error, results){
+       if(error) throw error;
+       if(results.length){
+           var author = results[0];
+           author.dob = author.dob.toString().split(' ').slice(0,4).join(' ');
+           author.dod = author.dod.toString().split(' ').slice(0,4).join(' ');
+           res.render('author_bio', {author: author});
+       }
+    });
+})
+
+
+// ADD NEW AUTHOR
+/* Create a new author - Get author information */
+app.get('/author/new', isAuthenticated, function(req, res){
+    res.render('author_new');
+});
+
+/* Create a new author - Add author into DBMS */
+app.post('/author/new', isAuthenticated, function(req, res){
+   //console.log(req.body);
+   connection.query('SELECT COUNT(*) FROM l9_author;', function(error, result){
+       if(error) throw error;
+       if(result.length){
+            var authorId = result[0]['COUNT(*)'] + 1;
+            var stmt = 'INSERT INTO l9_author ' +
+                      '(authorId, firstName, lastName, dob, dod, sex, profession, country, biography) '+
+                      'VALUES ' +
+                      '(' + 
+                       authorId + ',"' +
+                       req.body.firstname + '","' +
+                       req.body.lastname + '","' +
+                       req.body.dob + '","' +
+                       req.body.dod + '","' +
+                       req.body.sex + '","' +
+                       req.body.profession + '","' +
+                       req.body.country + '","' +
+                       req.body.biography + '"' +
+                       ');';
+            console.log(stmt);
+            connection.query(stmt, function(error, result){
+                if(error) throw error;
+                res.redirect('/author');
+            })
+       }
+   });
+});
+
+//EDIT AUTHOR
+/* Edit an author record - Display an author information */
+app.get('/author/:aid/edit', isAuthenticated, function(req, res){
+    console.log("AUTHOR ID:"); 
+    console.log(req.params.aid);
+    var stmt = 'SELECT * FROM l9_author WHERE authorId=' + req.params.aid + ';';
+    connection.query(stmt, function(error, results){
+       if(error){ 
+           console.log("error in /author/:aid/edit");
+           throw error;
+       }
+       if(results.length){
+           var author = results[0];
+           author.dob = author.dob.toISOString().split('T')[0];
+           author.dod = author.dod.toISOString().split('T')[0];
+           res.render('author_edit', {author: author});
+       }
+    });
+});
+
+/* Edit an author record - Update an author in DBMS */
+app.put('/author/:aid', isAuthenticated, function(req, res){
+    console.log(req.body);
+    var stmt = 'UPDATE l9_author SET ' +
+                'firstName = "'+ req.body.firstname + '",' +
+                'lastName = "'+ req.body.lastname + '",' +
+                'dob = "'+ req.body.dob + '",' +
+                'dod = "'+ req.body.dod + '",' +
+                'sex = "'+ req.body.sex + '",' +
+                'profession = "'+ req.body.profession + '",' +
+                'portrait = "'+ req.body.portrait + '",' +
+                'country = "'+ req.body.country + '",' +
+                'biography = "'+ req.body.biography + '"' +
+                'WHERE authorId = ' + req.params.aid + ";"
+    //console.log(stmt);
+    connection.query(stmt, function(error, result){
+        if(error) throw error;
+        res.redirect('/author/' + req.params.aid + "/bio");
+    });
+});
+
+// DELETE AUTHOR
+/* Delete an author record */
+app.get('/author/:aid/delete', isAuthenticated, function(req, res){
+    var stmt = 'DELETE from l9_author WHERE authorId='+ req.params.aid + ';';
+    connection.query(stmt, function(error, result){
+        if(error) throw error;
+        res.redirect('/author');
+    });
+});
+
+
+
+app.get("/author/:id/modal", function(req, res) {
     var stmt = "select * from l9_author where authorId=" + req.params.id + ";";
     connection.query(stmt, function(error, found){
 	    if(error) throw error;
 	    res.send(found);
 	});
-    
-
-    
 });
+
+
 
 /* The handler for undefined routes */
 app.get('*', function(req, res){
@@ -69,6 +221,23 @@ app.get('*', function(req, res){
 app.listen(process.env.PORT || 3000, function(){
     console.log('Server has been started');
 })
+
+
+
+function isAuthenticated(req, res, next){
+    if(!req.session.authenticated) res.redirect('/login');
+    else next();
+}
+
+
+function checkLoginCredentials(username, password) {
+    if (username != 'admin' || password != 'password') {
+        return false;
+    }
+    return true;
+}
+
+
 function getSQLStatement(req) {
     var stmt = "select * from l9_author, l9_quotes where "; 
     var phrase = null; 
--- public/script/app.js
+++ public/script/app.js
@@ -1,8 +1,35 @@
-$("a").on("click", function() {
+var deleteAuthorInfo = null; 
+var deleteAuthorTokens = null;
+$(".deleteAuthor").on("click", function() {
+    deleteAuthorInfo = this.id;
+    console.log(deleteAuthorInfo);
+    deleteAuthorTokens = deleteAuthorInfo.split(' ');
+    $("#confirmation").html("Are you sure you want to delete the author named " + deleteAuthorTokens.slice(0, deleteAuthorTokens.length-1).join(' ') + "? This cannot be undone.");
+}); 
+
+$("#modalDeleteButton").on("click", function() {
+    let deleteAuthorID = deleteAuthorInfo.split(' ')[deleteAuthorTokens.length-1];
+    console.log(deleteAuthorID);
+    $.ajax({
+        method: "GET",
+        url: "/author/" + deleteAuthorID + "/delete",
+        contentType: "application/json",
+        success: function(result,status) {
+            location.reload(true); //https://stackoverflow.com/questions/40286952/how-to-refresh-the-page-with-nodejs-after-click-on-submit-button
+        }, 
+        error: function(error, status) {
+            console.log("ERROR");
+            console.log(error);    
+        }
+    });//ajax
+    
+}); 
+
+$(".quotes").on("click", function() {
     var id = $(this).attr("id");
     $.ajax({
         method: "GET",
-        url: "/author/" + id,
+        url: "/author/" + id + "/modal",
         contentType: "application/json",
         data: { "id": id},
         success: function(result,status) {
@@ -34,5 +61,7 @@ $("a").on("click", function() {
     });//ajax
  
 }); 
+
+
 
     
